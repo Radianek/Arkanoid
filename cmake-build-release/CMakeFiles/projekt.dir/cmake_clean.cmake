445f2827387aef23125e023dbfc493f468e80431
Radianek <272315@student.pwr.edu.pl>
2023-05-17 14:31:01 +0200

    SFML działa

---
+++ cmake-build-release/CMakeFiles/projekt.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/projekt.dir/main.cpp.obj"
+  "CMakeFiles/projekt.dir/main.cpp.obj.d"
+  "libprojekt.dll.a"
+  "projekt.exe"
+  "projekt.exe.manifest"
+  "projekt.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/projekt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
